cd78f516d834058faa18bff391abc33728a12bf1
ADITYA JADHAV <145701971+SDFDW@users.noreply.github.com>
2026-01-09 12:57:54 +0530

    Add files via upload

---
+++ 6.2.py
@@ -0,0 +1,23 @@
+#
+n = 3
+
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i), end="")
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+
+#output
+#  1 
+# 1 2 
+#1 2 3 
+# 1 2 
+#  1 
